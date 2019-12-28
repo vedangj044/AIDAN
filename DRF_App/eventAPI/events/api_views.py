1118c9c0add7a20af4303e359e2ab22a3c0b8575
vedangj044 <vedangj044@gmail.com>
2019-12-28 17:10:49 +0530

    Changed queryset to 'get' events of the current user

--- DRF_App/eventAPI/events/api_views.py
+++ DRF_App/eventAPI/events/api_views.py
@@ -6,5 +6,10 @@ from rest_framework.permissions import IsAuthenticated
 class EventViewsets(viewsets.ModelViewSet):
 
     permission_classes = [IsAuthenticated]
-    queryset = models.eventdetails.objects.all()
     serializer_class = serializers.EventSerializer
+
+    def get_queryset(self):
+        user = self.request.user.id
+        queryset = models.eventdetails.objects.filter(user=user)
+
+        return queryset
--- DRF_App/eventAPI/events/serializers.py
+++ DRF_App/eventAPI/events/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from . import models
 
 class EventSerializer(serializers.ModelSerializer):
+
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = models.eventdetails
-        fields = ('name', 'place', 'time')
+        fields = ('id', 'user', 'name', 'place', 'time')
