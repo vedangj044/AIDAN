4af0a4f66f5d1253ea4e4ae62fd49762869660b1
vedangj044 <vedangj044@gmail.com>
2020-01-16 21:36:18 +0530

    RBAC Implemented

--- DRF_App/eventAPI/uploadapp/views.py
+++ DRF_App/eventAPI/uploadapp/views.py
@@ -1,25 +1,40 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import authentication, permissions
 from rest_framework import status
 from .serializers import FileSerializer
 from .models import File
 from django.core.files.base import ContentFile
 
+
 class FileUploadView(APIView):
 
-    def post(self, request, *args, **kwargs):
+    authentication_classes = [authentication.SessionAuthentication]
+
+    def get(self, request, *args, **kwargs):
 
         query = request.data.dict()['name']
 
         serializer = FileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
+        if request.user.is_superuser:
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                f = File.objects.filter(name=query)
+                if len(f)>0:
+                    f[0].content = request.data.dict()['content']
+                    f[0].save()
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.user.is_staff:
+            serializer.is_valid()
             f = File.objects.filter(name=query)
             if len(f)>0:
                 f[0].content = request.data.dict()['content']
                 f[0].save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response("{'Message': 'Login not found'}", status=status.HTTP_400_BAD_REQUEST)
