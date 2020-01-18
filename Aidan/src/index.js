04728f8df3ba218042eade55133c9463785a4ee0
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-01-18 11:42:09 +0530

    Login and Signin Page

--- Aidan/src/index.js
+++ Aidan/src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+// import App from '../App';
 import * as serviceWorker from './serviceWorker';
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router";
 import Dashboard from "./components/Dashboard";
 import Signin from "./components/Signin";
-// import  from "./components/Dashboard.js";
+import Admin from "./components/Admin.js";
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(<Router history={hist}>
     <Switch>
       <Route path="/signin" component={Signin} />
+      <Route path="/admin" component={Admin} />
       <Route path="/" component={Dashboard} />
-
-      {/* <Route path="/blog-posts" component={BlogPostsPage} /> */}
     </Switch>
   </Router>,
   document.getElementById("root"));
